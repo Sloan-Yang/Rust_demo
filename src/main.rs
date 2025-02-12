171da95ecc986de372de986fe5f00b099a06168f
Sloan Yang <yangshr2024@shanghaitech.edu.cn>
2025-02-13 07:49:55 +0800

    add move system

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,42 @@
 use bevy::prelude::*;
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // 2D 摄像机
-    commands.spawn(Camera2d::default());
+#[derive(Component)]
+struct Player;
 
-    // 加载图片并创建精灵
-    commands.spawn(Sprite {
-        image: asset_server.load("image.jpg"),
-        ..Default::default()
-    });
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d::default());
+    commands
+        .spawn(Sprite {
+            image: asset_server.load("image.jpg"),
+            ..Default::default()
+        })
+        .insert(Player);
 }
 
-fn keyboard_input_system(keyboard_input: Res<ButtonInput<KeyCode>>) {
+fn move_system(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut players: Query<(&mut Transform ,  &mut Sprite ),  With<Player>>,
+) {
+
+    let (mut player, mut sprite) = players.iter_mut().next().unwrap();
     if keyboard_input.pressed(KeyCode::KeyA) {
-        println!("Go Left!");
+        sprite.flip_x = false;
+        player.translation.x -= 1.0;
+        
     }
     if keyboard_input.pressed(KeyCode::KeyD) {
-        println!("Go Right!");
+        sprite.flip_x = true;
+        player.translation.x += 1.0;
+        
     }
 }
+
+
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update , keyboard_input_system)
+        .add_systems(Update , move_system)
         .run();
 }
